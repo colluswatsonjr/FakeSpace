3503b0048ba61a4590c6274e766379e286093966
Collus Watson <cklwatsonjr@gmail.com>
2022-12-06 18:22:05 -0500

    'create routes to handle requests'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :posts
   resources :pages
   resources :users
+  
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

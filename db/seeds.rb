6fd13dd4d2cb1a1ce13af5a51844abdcb8998aed
Collus Watson <cklwatsonjr@gmail.com>
2022-12-06 13:04:01 -0500

    'create seeds for models'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "📃 Seeding data..."
+
+User.create(username: 'hearLouis', first_name: 'Louis', last_name: 'Watson', password_digest: 'default')
+User.create(username: 'seeTre', first_name: 'Tre', last_name: 'Watson', password_digest: 'default')
+User.create(username: 'speakCollus', first_name: 'Junior', last_name: 'Watson', password_digest: 'default')
+
+Page.create(title: 'Comedy', bio:'This page is dedicated to Comedy' )
+Page.create(title: 'Horror', bio:'This page is dedicated to Horror' )
+Page.create(title: 'Action', bio:'This page is dedicated to Action' )
+
+Post.create(user_id: 1, page_id: 1, text:'This is random')
+Post.create(user_id: 1, page_id: 3, text:'This is random')
+Post.create(user_id: 2, page_id: 1, text:'This is random')
+Post.create(user_id: 2, page_id: 2, text:'This is random')
+Post.create(user_id: 3, page_id: 2, text:'This is random')
+Post.create(user_id: 3, page_id: 3, text:'This is random')
+
+puts "✅ Done seeding"

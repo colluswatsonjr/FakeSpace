235a5708e8cd20db3c12a97f2c761b7a78e955df
Collus Watson <cklwatsonjr@gmail.com>
2022-12-07 19:16:05 -0500

    'create display posts component to display posts'

---
+++ client/src/components/DisplayPosts.js
@@ -0,0 +1,39 @@
+
+
+function DisplayPosts({ userId, setPosts, posts }) {
+
+    function handleDelete(id) {
+        fetch(`/posts/${id}`, {
+            method: "DELETE",
+        }).then((r) => {
+            if (r.ok) {
+                const updatePosts = posts.filter((post) => post.id !== id);
+                setPosts(updatePosts);
+            }
+        });
+    }
+
+    function DisplayDeleteBtn(post) {
+        if (post.username === userId) {
+            return <button onClick={() => handleDelete(post.id)}>x</button>
+        } else {
+            console.log(false)
+        }
+    }
+
+    return (
+        <div>
+            <h3>Posts</h3>
+            {posts.map((post) => {
+                return (
+                    <div key={post.id}>
+                        {post.text + ' // ' + post.username}
+                        {DisplayDeleteBtn(post)}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default DisplayPosts

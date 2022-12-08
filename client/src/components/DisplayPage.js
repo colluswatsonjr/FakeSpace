f36cc61b4fb561113415ccb6c938484e175eb3b3
Collus Watson <cklwatsonjr@gmail.com>
2022-12-08 13:43:13 -0500

    'display page data and back button'

--- client/src/components/DisplayPage.js
+++ client/src/components/DisplayPage.js
@@ -5,21 +5,19 @@ import DisplayPosts from "./DisplayPosts"
 
 function DisplayPage({ user, page, setPage }) {
 
-    console.log(user, page)
-
     const [posts, setPosts] = useState(page.posts)
 
     return (
         <div>
             <h3>Page</h3>
 
-            <div key={page.id} onClick={() => console.log(page)}>
+            <div key={page.id}>
                 <h4>{page.title}</h4>
                 <p>{page.bio}</p>
-                <button onClick={setPage}>Back</button>
+                <button onClick={()=>setPage()}>Back</button>
             </div>
 
-            <CreatePost pageId={page.id} posts={posts} setPosts={setPosts}/>
+            <CreatePost pageId={page.id} posts={posts} setPosts={(x)=>setPosts(x)}/>
             <DisplayPosts userId={user.username} posts={posts} setPosts={setPosts}/>
 
         </div>

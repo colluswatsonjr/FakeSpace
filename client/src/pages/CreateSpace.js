6521d32271f862459e42b3eb2bd81650d6eb0849
Collus Watson <cklwatsonjr@gmail.com>
2022-12-08 19:21:52 -0500

    'create page route'

--- client/src/pages/CreateSpace.js
+++ client/src/pages/CreateSpace.js
@@ -1,29 +1,30 @@
 import { useState } from "react"
-import DisplayPage from "../components/DisplayPage"
-
 
 function CreateSpace({ user }) {
-    const [page, setPage] = useState(null)
+
     const [form, setForm] = useState({ title: '', bio: '' })
+    
     function handleSubmit(e) {
         e.preventDefault()
-        // setPage(form)
-        // fetch("/pages", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify(form)
-        // })
-        //     .then((res) => {
-        //         if (res.ok) {
-        //             res.json().then((page) => { setPage(page) });
-        //         } else {
-        //             res.json().then((err) => console.log(err))
-        //         }
-        // })
+        console.log(form)
+
+        fetch("/pages", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((page) => console.log(page));
+                } else {
+                    res.json().then((err) => console.log(err))
+                }
+        })
     }
-    // if (page) return <DisplayPage user={user} page={page} setPage={() => setPage(null)} />;
+    // console.log(page)
+    // if (page) return <DisplayPage user={user} page={page} />;
 
     return (
         <div>

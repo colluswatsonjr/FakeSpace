f8b8dfeaf1f085eda6f608b5d1836efbcd6a9a57
Collus Watson <cklwatsonjr@gmail.com>
2022-12-13 11:04:21 -0500

    'move create post to be handled on DisplayPage.js'

--- client/src/components/DisplayPage.js
+++ client/src/components/DisplayPage.js
@@ -1,11 +1,34 @@
 import { useState } from "react"
-import CreatePost from "./CreatePost"
-import DisplayPosts from "./DisplayPosts"
 
+import DisplayPosts from "./DisplayPosts"
 
 function DisplayPage({ user, page, setPage }) {
 
     const [posts, setPosts] = useState(page.posts)
+    const [form, setForm] = useState({ page_id: page.id, text: '' })
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        fetch("/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((post) => {
+                        setPosts([...posts, post])
+                    });
+                } else {
+                    res.json().then((err) => console.log(err))
+                }
+            })
+        setForm({ page_id: page.id, text: '' })
+    }
 
     return (
         <div>
@@ -14,11 +37,15 @@ function DisplayPage({ user, page, setPage }) {
             <div key={page.id}>
                 <h4>{page.title}</h4>
                 <p>{page.bio}</p>
-                <button onClick={()=>setPage()}>Back</button>
+                <button onClick={() => setPage()}>Back</button>
             </div>
 
-            <CreatePost pageId={page.id} posts={posts} setPosts={(x)=>setPosts(x)}/>
-            <DisplayPosts userId={user.username} posts={posts} setPosts={setPosts}/>
+            <form>
+                <input type='text' placeholder="add post..." value={form.text} onChange={(e) => { setForm({ ...form, text: e.target.value }) }} />
+                <button onClick={(e) => handleSubmit(e)}>Submit</button>
+            </form>
+
+            <DisplayPosts userId={user.username} posts={posts} setPosts={setPosts} />
 
         </div>
     )
